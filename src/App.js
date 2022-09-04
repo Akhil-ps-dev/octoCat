5344fd389336034716c141a1f5f9d5c875c2ef2d
Chatter-Boo <psakhil211@gmail.com>
2022-09-04 15:12:23 +0530

    set up Third party auth

--- src/App.js
+++ src/App.js
@@ -1,46 +1,57 @@
-import './App.css';
-import React from 'react';
-import {BrowserRouter , Route, Routes} from 'react-router-dom';
-import SuperTokens, { SuperTokensWrapper, getSuperTokensRoutesForReactRouterDom } from "supertokens-auth-react";
-import ThirdPartyEmailPassword, {Google, } from "supertokens-auth-react/recipe/thirdpartyemailpassword";
+import "./App.css";
+import Home from "./Home";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SuperTokens, {
+  SuperTokensWrapper,
+  getSuperTokensRoutesForReactRouterDom,
+} from "supertokens-auth-react";
+import ThirdPartyEmailPassword, {
+  Google,
+  ThirdPartyEmailPasswordAuth,
+} from "supertokens-auth-react/recipe/thirdpartyemailpassword";
 import Session from "supertokens-auth-react/recipe/session";
 import * as reactRouterDom from "react-router-dom";
 
 SuperTokens.init({
-    appInfo: {
-        // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
-        appName: "super-token",
-        apiDomain: "http://localhost:3001",
-        websiteDomain: "http://localhost:3000",
-    },
-    recipeList: [
-        ThirdPartyEmailPassword.init({
-            signInAndUpFeature: {
-                providers: [
-                    // Github.init(),
-                    Google.init(),
-                    // Facebook.init(),
-                    // Apple.init(),
-                ]
-            }
-        }),
-        Session.init()
-    ]
+  appInfo: {
+    appName: "super-token",
+    apiDomain: "http://localhost:3001",
+    websiteDomain: "http://localhost:3000",
+  },
+  recipeList: [
+    ThirdPartyEmailPassword.init({
+      signInAndUpFeature: {
+        providers: [
+          // Github.init(),
+          Google.init(),
+          // Facebook.init(),
+          // Apple.init(),
+        ],
+      },
+    }),
+    Session.init(),
+  ],
 });
 
 class App extends React.Component {
   render() {
-      return (
-          <SuperTokensWrapper>
-              <BrowserRouter>
-                  <Routes>
-                      {/*This renders the login UI on the /auth route*/}
-                      {getSuperTokensRoutesForReactRouterDom(reactRouterDom)}
-                      {/*Your app routes*/}
-                  </Routes>
-              </BrowserRouter>
-          </SuperTokensWrapper>
-      );
+    return (
+      <SuperTokensWrapper>
+      
+        <BrowserRouter>
+          <Routes >
+            {getSuperTokensRoutesForReactRouterDom(reactRouterDom)}
+
+            //! This is the route that will be rendered when the user is logged in
+       <Route path="/" element={<Home/>} />
+
+      
+           
+          </Routes>
+        </BrowserRouter>
+      </SuperTokensWrapper>
+    );
   }
 }
 export default App;
